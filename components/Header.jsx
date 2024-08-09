14e6d905516c259937ef845fe5a00e6fd55f28dc
iRxllz <vrollandvass@yahoo.com>
2024-08-09 15:18:55 +0300

    Small header fix

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     return <header className='py-8 xl:py-12 text-white'>
         <div className="container mx-auto flex justify-between items-center">
             <Link href="/">
-                <h1 className='text-4xl font-semibold'>
+                <h1 className='text-3xl font-semibold'>
                     Rolland Vass<span className='text-accent'>.</span>
                 </h1>
             </Link>
